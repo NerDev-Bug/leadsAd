dd68c3270ba2b25e4bbfc3b62f20b8587c5e1be2
NerDev-Bug <jhonalfietorzar0987@gmail.com>
2025-09-26 21:40:31 +0800

    addddddd 404 not found

--- resources/js/Pages/Errors/NotFound.vue
+++ resources/js/Pages/Errors/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
-      <h5 class="text-[28px] md:text-[50px] font-bold text-red-600 mb-6">404 Not Found</h5>
-      <p class="text-xl text-gray-700 mb-8">Oops! The page you are looking for does not exist.</p>
+      <h5 class="text-[28px] md:text-[50px] font-bold text-black mb-6">This page isn't availabe</h5>
+      <p class="text-xl text-gray-700 mb-8">The link you followed may be broken, or the page may not existed</p>
       <button
         @click="goLogin"
         class="px-6 py-3 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700"
